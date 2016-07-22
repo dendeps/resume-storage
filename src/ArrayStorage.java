261799418768c7b0b6c75c7e52023dd1b57a5dd0
dendeps <deps@ua.fm>
2016-07-22 16:21:22 +0800

    Add files via upload

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,80 @@
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.ArrayList;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for(int i=0;i<storage.length;i++){
+            storage[i]=null;
+        }
+        size = 0;
+
+        System.out.println("Cleared");
     }
 
     void save(Resume r) {
+        for(int i=0;i<storage.length;i++){
+            if(storage[i]==null){
+                storage[i]=r;
+                size++;
+
+                System.out.println(storage[i].uuid+" Saved with index "+i);
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for(int i=0;i<storage.length;i++){
+            if((storage[i]!=null) && uuid.equals(storage[i].uuid)){
+                return storage[i];
+            }
+        }
+        System.out.println(uuid+" not found");
         return null;
     }
 
     void delete(String uuid) {
+        for(int i=0;i<storage.length;i++){
+            if((storage[i]!=null) && uuid.equals(storage[i].uuid)){
+                storage[i]=null;
+                size--;
+                System.out.println("Deleted: "+ uuid);
+                return;
+            }
+        }
+        System.out.println(uuid+" not found");
+        return;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+
     Resume[] getAll() {
-        return new Resume[0];
+
+        Resume[] allResume = new Resume[size];
+
+        int k=0;
+
+        for(int i=0;i<storage.length;i++){
+            if(storage[i]!=null){
+                allResume[k]=storage[i];
+                k++;
+            }
+        }
+
+        return allResume;
+
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
--- src/MainArray.java
+++ src/MainArray.java
@@ -12,7 +12,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (1 = list | 2. size. | 3. save uuid | 4. delete uuid | 5. get uuid | 6. clear | 7. exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
@@ -22,31 +22,32 @@ public class MainArray {
             if (params.length == 2) {
                 uuid = params[1].intern();
             }
-            switch (params[0]) {
-                case "list":
+
+            switch ( Integer.parseInt(params[0])) {
+                case 1: //"list":
                     printAll();
                     break;
-                case "size":
+                case 2: //"size":
                     System.out.println(ARRAY_STORAGE.size());
                     break;
-                case "save":
+                case 3: //"save":
                     r = new Resume();
                     r.uuid = uuid;
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
-                case "delete":
+                case 4: //"delete":
                     ARRAY_STORAGE.delete(uuid);
                     printAll();
                     break;
-                case "get":
+                case 5: //"get":
                     System.out.println(ARRAY_STORAGE.get(uuid));
                     break;
-                case "clear":
+                case 6: //"clear":
                     ARRAY_STORAGE.clear();
                     printAll();
                     break;
-                case "exit":
+                case 7: //"exit":
                     return;
                 default:
                     System.out.println("Неверная команда.");
